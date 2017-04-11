07e7aca41a69650ddb13f154acf66f7af3cdfb50
zc <zc_93@qq.com>
2017-04-12 01:36:26 +0800

    表单控件

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="test">
+  <div class="test" v-bind:style="{marginBottom:'100px'}">
     <h1>{{ msg }}</h1>
     <h2 v-bind:id="dynamicId">Essential Links</h2>
     <ul>
@@ -25,6 +25,46 @@
         <div v-bind:style="{color:fontColor}">style绑定</div>
         <div v-bind:style="[style1,style2]">对象绑定</div>
     </div>
+    <div>
+      <h3>v-for</h3>
+      <ul id='v-for-1'>
+        <li v-for="(item,index) in items">
+        {{dynamicId}}:{{index}}:{{item.message}}
+        </li>
+      </ul>
+      <h3>v-for遍历对象</h3>
+      <ul>
+        <li v-for="(value,key,index) in object">
+          {{index}} {{key}} : {{value}}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <button v-on:click="greet">Greet</button>
+    </div>
+    <div>
+      <h3>表单</h3>
+      <input v-model="msg" placeholder="请输入">
+      <p>Message is: {{ msg }}</p>
+      <textarea v-if="seen" v-model="msg" placeholder="add multiple lines"></textarea>
+      <input id="checkbox1" type="checkbox" v-model="seen">
+      <label for="checkbox1">{{seen?'可见':'有些元素被隐藏了'}}</label>
+      <div>
+        <span>多选：</span>
+        <input type="checkbox" id="checkgroup1" v-model="checkedItems" value="item1">
+        <label for="checkgroup1">item1</label>
+        <input type="checkbox" id="checkgroup2" v-model="checkedItems" value="item2">
+        <label for="checkgroup2">item2</label>
+        <input type="checkbox" id="checkgroup3" v-model="checkedItems" value="item3">
+        <label for="checkgroup3">item3</label>
+        <input type="checkbox" id="checkgroup4" v-model="checkedItems" value="item4">
+        <label for="checkgroup4">item4</label>
+        <br><span>已选择：{{checkedItems}}</span>
+        <ul>
+          <li v-bind:style="{display:'block',border:'1px solid',margin:'2px'}" v-for="item in checkedItems">{{item}}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -46,7 +86,14 @@ export default {
       },
       style2:{
         fontWeight:'bold'
-      }
+      },
+      items:[
+        {message:'A'},{message:'B'}
+      ],
+      object:{
+        First:'AAA',Last:'BBB',And:'CCC'
+      },
+      checkedItems:[]
     }
   },
   computed:{
@@ -57,6 +104,12 @@ export default {
           return Date.now();
       }
   },
+  methods:{
+    greet:function(e){
+      alert('Hello'+this.msg);
+      alert(e.target.tagName);
+    }
+  }
 }
 </script>
 
